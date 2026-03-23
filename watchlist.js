675223fd2a98f43cfa4d03e8ceef1c06bf4ff56e
Pratik <sharmapratik789@gmail.com>
2026-03-23 12:18:01 +0530

    added remove button in watchlist list

--- watchlist.js
+++ watchlist.js
@@ -11,4 +11,44 @@ if (savedMovie.length === 0) {
     
     </div>
     `
+} else {
+    async function rendorWatchlist() {
+        let watchlistMoviesHTML = ""
+        for (let id of savedMovie) {
+            const response = await fetch(`https://www.omdbapi.com/?i=${id}&apikey=6a034881`)
+            const moviesData = await response.json()
+            console.log(moviesData)
+            let moviesHTML = `
+            <div class="movie-card">
+                        <img src="${moviesData.Poster}" class="movie-poster" />
+                        <div class="movie-info">
+                            <div class="movie-header">
+                                <h3>${moviesData.Title}</h3>
+                                <span class="rating">⭐️ ${moviesData.imdbRating}</span>
+                            </div>
+                            <div class="movie-metadata">
+                                <span>${moviesData.Runtime}</span>
+                                <span>${moviesData.Genre}</span>
+                                <button class="watchlist-btn" onClick="removeFromWatchlist('${moviesData.imdbID}')">
+                                <img src="image/icon1.png" class="plus-icon" />
+                                Remove
+                                </button>
+                            </div>
+                                <p class="moive-plot">${moviesData.Plot}</p>
+                        </div>
+                    </div>
+                    <hr>
+            `
+            watchlistMoviesHTML += moviesHTML
+        }
+        watchlistContainer.innerHTML += watchlistMoviesHTML
+    }
+    rendorWatchlist()
+}
+
+
+window.removeFromWatchlist = function (id) {
+    savedMovie.pop(id)
+    localStorage.setItem("watchlist", JSON.stringify(savedMovie))
+    console.log(id)
 }
